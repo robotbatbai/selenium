3e118ee112f05b1aade5d93eadf04bafaf14ce7f
anh-luu <anh-luu@dac.co.jp>
2018-11-15 18:44:56 +0900

    add sele

--- demo.py
+++ demo.py
@@ -1,13 +1,21 @@
-
+import pickle
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+browser = webdriver.Firefox()
+
+def get_cookies():
+    browser.get("https://login.aliexpress.com/buyer.htm?return=https%3A%2F%2Fwww.aliexpress.com%2F&random=CEA73DF4D81fdsa75227F78080B9B6126")
+    print('input your username and passowrd in firefox and hit submit')
+    raw_input('Hit enter here if you summited the form: <Enter>')
+    cookies= browser.get_cookies()
+    pickle.dump(cookies,open("cookies.pickle","wb"))
+
+def set_cookies():
+    browser.get("https://aliexpress.com")
+    cookies = pickle.load(open("cookies.pickle","wb"))
+    for cookie in cookies:
+        browser.add_cookie(cookie)
+    browser.get("https://bestselling.aliexpress.com/en")
+
+if __name__ == '__main__':
+    get_cookies()
