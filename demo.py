ab33ddaa1dc0e33e16edb43dca96483046340d0a
robot <robotbatbai@gmail.com>
2018-11-17 22:25:27 +0900

    no message

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ browser = webdriver.Firefox()
 def get_cookies():
     browser.get("https://login.aliexpress.com/buyer.htm?return=https%3A%2F%2Fwww.aliexpress.com%2F&random=CEA73DF4D81fdsa75227F78080B9B6126")
     print('input your username and passowrd in firefox and hit submit')
-    raw_input('Hit enter here if you summited the form: <Enter>')
+    input('Hit enter here if you summited the form: <Enter>')
     cookies= browser.get_cookies()
     pickle.dump(cookies,open("cookies.pickle","wb"))
 
